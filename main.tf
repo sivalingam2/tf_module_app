5cab6c4990c0eaa81a5583475ef182830651085a
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-14 21:53:10 +0530

    setting mongodb cluster

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
+  name = "${local.name_prefix}-asg"
   vpc_zone_identifier = var.subnet_ids
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
