80c5bc3c66196c6cf7e15f256f2dbcba82c5ac2a
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-18 21:57:50 +0530

    setting up load balancer

--- main.tf
+++ main.tf
@@ -120,42 +120,56 @@ resource "aws_lb_listener_rule" "public" {
     }
   }
 }
-#resource "aws_iam_policy" "main" {
-#  name        = "${local.name_prefix}-policy"
-#  path        = "/"
-#  description = "${local.name_prefix}-policy"
-#
-#  policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = [
-#          "ec2:Describe*",
-#        ]
-#        Effect   = "Allow"
-#        Resource = "*"
-#      },
-#    ]
-#  })
-#}
-#resource "aws_iam_role" "main" {
-#  name = "${local.name_prefix}-policy"
-#
-#  # Terraform's "jsonencode" function converts a
-#  # Terraform expression result to valid JSON syntax.
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#
-#  tags = merge(local.tags, {Name = "${local.name_prefix}-role" })
-#}
+resource "aws_iam_policy" "main" {
+  name        = "${local.name_prefix}-policy"
+  path        = "/"
+  description = "${local.name_prefix}-policy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:372609574969:parameter/docudb.dev.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+resource "aws_iam_role" "main" {
+  name = "${local.name_prefix}-policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, {Name = "${local.name_prefix}-role" })
+}
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
-pip3.11 install botocre boto3 &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
