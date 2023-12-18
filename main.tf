6342495401c4680e4620d26736d8ec687f41eb53
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-18 08:47:03 +0530

    setting up load balancer

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_lb_target_group" "public" {
 resource "aws_lb_target_group_attachment" "public" {
   count           = length(data.dns_a_record_set.private_alb_name.addrs)
   target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = element(data.dns_a_record_set.private_alb_name.addrs, count.index )
+  target_id        = element(tolist(data.dns_a_record_set.private_alb_name.addrs), count.index )
   port             = 80
 }
 #resource "aws_lb_listener_rule" "main" {
