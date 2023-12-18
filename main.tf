fb981604db20e741ebbb689ea630ceb6cd1378d1
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-18 14:51:57 +0530

    setting up load balancer

--- main.tf
+++ main.tf
@@ -96,27 +96,27 @@ resource "aws_lb_target_group" "public" {
   protocol = "HTTP"
   vpc_id   = var.default_vpc_id
 }
-resource "aws_lb_target_group_attachment" "public" {
-  count           = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
-  target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index )
-  port             = 80
-  availability_zone = "all"
-}
-resource "aws_lb_listener_rule" "public" {
-  count =  var.component == "frontend" ? 1 : 0
-  listener_arn = var.public_lb_listener
-  priority     = var.priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.public[0].arn
-  }
-
-
-  condition {
-    host_header {
-      values = ["${var.env}.sivadevops22.online"]
-    }
-  }
-}
+#resource "aws_lb_target_group_attachment" "public" {
+#  count           = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+#  target_group_arn = aws_lb_target_group.public[0].arn
+#  target_id        = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index )
+#  port             = 80
+#  availability_zone = "all"
+#}
+#resource "aws_lb_listener_rule" "public" {
+#  count =  var.component == "frontend" ? 1 : 0
+#  listener_arn = var.public_lb_listener
+#  priority     = var.priority
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.public[0].arn
+#  }
+#
+#
+#  condition {
+#    host_header {
+#      values = ["${var.env}.sivadevops22.online"]
+#    }
+#  }
+#}
