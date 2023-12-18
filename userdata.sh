d33108b2e53115f2e41cd13ab5df17c4bd3bb7e5
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-18 10:42:33 +0530

    setting up load balancer

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 yum install ansible -y &>>/opt/userdata.log
 
 ansible-pull -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=${component} &>>/opt/userdata.log
